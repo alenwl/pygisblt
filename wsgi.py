8b3b83b68e36080368ae04afb4907a4696a8b57d
Ander <ander.larranaga@enwl.co.uk>
2020-10-16 14:44:47 +0100

    Adding config files

--- wsgi.py
+++ wsgi.py
@@ -2,18 +2,20 @@ from flask import Flask
 import time
 from src.mysql_database_modules.db_queue_monitor import *
 application = Flask(__name__)
+import sys
 
 @application.route("/")
 def hello():
     return "Hello World!"
 
-@application.route('/test/')
+@application.route('/dbtest/')
 def test_al():
     test = db_queue_monitor()
     if test.check_connectivity():
-        return("Success")
+        #return("Successful connection to db")
+        return(sys.path)
     else:
-        return("Fail")
+        return("Failed connecting to db")
 
 if __name__ == "__main__":
     application.run()
