ad937e452df9bb607b08d64ff23fa47cd5ab016d
Ander <ander.larranaga@enwl.co.uk>
2020-11-02 15:47:41 +0000

    Testing xml module

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 # Required modules
 from flask import Flask
-from flask import requests
+from flask import request
 import time
 from src.mysql_database_modules.db_queue_monitor import *
 from src.blt_generation_modules.iccp_config_generator import *
