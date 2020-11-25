360a7836399698911fdc87c56ff74c48bfb63ffd
Ander <ander.larranaga@enwl.co.uk>
2020-11-25 15:12:19 +0000

    testing api key and client id validation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## testing web api with postman
 GET to http://pygisblt-gis-blt-gen.192.168.99.100.nip.io/api?type=
 Type could be rcc,iccp or network_config
-add API-Key to header
+add API-Key to header and client-id
 
 ## on openshift
 minishift start
--- src/authentication/validate_client.py
+++ src/authentication/validate_client.py
@@ -9,6 +9,8 @@ class validate_client():
         if not key or not clid:
             return False
         else:
+            # Hash api-key on request header and compare
+            # with hash stored for client-id in credentials table
             if (sha256(key.encode('utf-8')).hexdigest() 
                     == 'badc551bb4285e28227b7361e6056b2ed119f23fb1c225a070805c160d7e234e'):
                 return True
